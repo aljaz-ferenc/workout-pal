fbd0945d3761226af904e4a33bf40f58367aac25
aljaz-ferenc <aljaz.ferenc@protonmail.com>
2023-06-26 19:50:44 +0200

    fix bug

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -13,7 +13,7 @@ const sendToken = (user, statusCode, res) => {
     const cookieOptions = {
         expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
         httpOnly: true,
-         secure: true
+         secure: false
     }
 
     res.cookie('workouts', token, cookieOptions)
